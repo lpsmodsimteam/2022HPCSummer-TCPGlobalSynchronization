6a31dd37c4d445d50b861d0f28bf962c0a970a11
Nicholas Schantz <nickjohnschantz@gmail.com>
2022-07-19 10:33:58 -0400

    Python Commenting

--- tests/tcpGlobSync.py
+++ tests/tcpGlobSync.py
@@ -1,14 +1,15 @@
-import sst
+import sst # Use SST Library
 
+# Creating a sender component from element tcpGlobSync (tcpGlobSync.sender) named "Sender Zero."
 sender_zero = sst.Component("Sender Zero", "tcpGlobSync.sender")
 sender_zero.addParams(
     {
-        "tickFreq": "1s",
-        "low_send_rate": "10",
-        "max_send_rate": "50",
-        "verbose_level": "1",
-        "node_id": "0",
-        "starting_cycle": "1",
+        "tickFreq": "1s", # frequency component updates at.
+        "low_send_rate": "10", # minimum send rate of sender and what it will limit its send rate to.
+        "max_send_rate": "50", # maximum send rate of sender
+        "verbose_level": "1", # level of console verbosity.
+        "node_id": "0", # id of node.
+        "starting_cycle": "1", # cycle in which component begins to send packets.
     }
 )
 
@@ -39,15 +40,16 @@ sender_two.addParams(
 receiver = sst.Component("Receiver Node", "tcpGlobSync.receiver")
 receiver.addParams(
     {
-        "tickFreq": "1s",
-        "process_rate": "100",
-        "queue_size": "145",
-        "verbose_level": "1",
-        "num_nodes": "3",
-        "window_size": "100",
+        "tickFreq": "1s", # frequency component updates at.
+        "process_rate": "100", # number of packets consumed per tick.
+        "queue_size": "145", # size of queue.
+        "verbose_level": "1", # level of console verbosity.
+        "num_nodes": "3", # number of nodes that will connect to receiver.
+        "window_size": "100", # window size (in sim time) for global sync detection.
     }
 )
 
+# Connect the senders to one of the receiver's ports.
 sst.Link("Link_Zero").connect((sender_zero, "port", "1ms"), (receiver, "port0", "1ms"))
 sst.Link("Link_One").connect((sender_one, "port", "1ms"), (receiver, "port1", "1ms"))
 sst.Link("Link_Two").connect((sender_two, "port", "1ms"), (receiver, "port2", "1ms"))
